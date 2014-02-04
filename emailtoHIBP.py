88bd6466940d21d52c0d5235ace10b6a97d69d46
SudhanshuC <sudhanshuchauhan007@yahoo.com>
2014-02-04 17:19:03 +0530

    Create emailtoHIBP.py
    Email To HaveIBeenPwned tansform for Maltego

---
+++ emailtoHIBP.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+   
+#EmailtoHIBP.py
+#Author: Sudhanshu Chauhan - @Sudhanshu_C
+     
+#This Script will retrieve the Domain(s) at which the specified account has been compromised
+#It uses the API provided by https://haveibeenpwned.com/
+#Special Thanks to Troy Hunt - http://www.troyhunt.com/
+#For MaltegoTransform library and Installation guidelines go to http://www.paterva.com/web6/documentation/developer-local.php
+   
+from MaltegoTransform import *
+import sys
+import urllib2
+mt = MaltegoTransform();
+mt.parseArguments(sys.argv);
+email=mt.getValue();
+mt = MaltegoTransform()
+hibp="https://haveibeenpwned.com/api/breachedaccount/"
+getrequrl=hibp+email
+response = urllib2.urlopen(getrequrl)
+for rep in response:
+  mt.addEntity("maltego.Phrase","Pwned at " + rep)
+mt.returnOutput()
+
