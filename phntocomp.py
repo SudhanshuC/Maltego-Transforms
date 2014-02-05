4e2a72021e2ada83e53e501d69ed1acc84751b87
SudhanshuC <sudhanshuchauhan007@yahoo.com>
2014-02-05 13:01:14 +0530

    Create phntocomp.py
    Phone Number to Company transform for Maltego.

---
+++ phntocomp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+ 
+#phntocomp.py
+#Author: Sudhanshu Chauhan - @Sudhanshu_C
+ 
+#This Script will retrieve the Domain(s) at which the specified account has been compromised
+#It uses the API provided by https://www.opencnam.com
+ 
+from MaltegoTransform import *
+import sys
+import urllib2
+mt = MaltegoTransform()
+mt.parseArguments(sys.argv)
+phn=mt.getValue()
+phn=phn.replace(' ','')
+mt = MaltegoTransform()
+opencnam="https://api.opencnam.com/v2/phone/"
+getrequrl=opencnam+phn
+response = urllib2.urlopen(getrequrl)
+mt.addEntity("maltego.Phrase", response.read())
+mt.returnOutput()
