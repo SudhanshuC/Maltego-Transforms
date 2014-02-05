6cf47d4800ed49aecd12593fbfc5907090d5f6e9
SudhanshuC <sudhanshuchauhan007@yahoo.com>
2014-02-05 17:44:58 +0530

    Create reverseipdomain.py
    Reverse IP Domain Check.

---
+++ reverseipdomain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+ 
+#reverseipdomain.py
+#Author: Sudhanshu Chauhan - @Sudhanshu_C
+ 
+#This Script will perform a reverse IP domain check
+#http://www.yougetsignal.com
+ 
+from MaltegoTransform import *
+import sys
+import urllib2
+import re
+mt = MaltegoTransform()
+mt.parseArguments(sys.argv)
+url=mt.getValue()
+mt = MaltegoTransform()
+opencnam="http://domains.yougetsignal.com/domains.php?remoteAddress="
+getrequrl=opencnam+url
+print getrequrl
+header={'User-Agent':'Mozilla',}
+req=urllib2.Request(getrequrl,None,header)
+response = urllib2.urlopen(req)
+print response.read()
+ 
+domains =re.findall("((?:[0-9]*[a-z][a-z\\.\\d\\-]+)\\.(?:[0-9]*[a-z][a-z\\-]+))(?![\\w\\.])",response.read())
+for domain in domains:
+  mt.addEntity("maltego.Phrase", domain)
+mt.returnOutput()
