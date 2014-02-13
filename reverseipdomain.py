87fe8948f926a45285c2e5d7a4d84121c814d670
SudhanshuC <sudhanshuchauhan007@yahoo.com>
2014-02-13 13:11:19 +0530

    Update reverseipdomain.py

--- reverseipdomain.py
+++ reverseipdomain.py
@@ -10,7 +10,6 @@ from MaltegoTransform import *
 import sys
 import urllib2
 import re
-from os.path import exists
 mt = MaltegoTransform()
 mt.parseArguments(sys.argv)
 url=mt.getValue()
@@ -19,20 +18,8 @@ opencnam="http://domains.yougetsignal.com/domains.php?remoteAddress="
 getrequrl=opencnam+url
 header={'User-Agent':'Mozilla',}
 req=urllib2.Request(getrequrl,None,header)
-
-if exists('reverseipdomainop.txt'):
-  flt=open('reverseipdomainoptemp.txt','wr')
-  data=flt.read()
-  for d in data:
-    mt.addEntity("maltego.Domain", d)
-
-else:
-  response = urllib2.urlopen(req)
-  domains =re.findall("((?:[0-9]*[a-z][a-z\\.\\d\\-]+)\\.(?:[0-9]*[a-z][a-z\\-]+))(?![\\w\\.])",response.read())
-  fl=open('reverseipdomainop.txt','a')
-  flt=open('reverseipdomainoptemp.txt','a')
-  for domain in domains:
-    fl.write(domain)
-    mt.addEntity("maltego.Domain", domain)
-
+response=urllib2.urlopen(req)
+domains=re.findall("((?:[0-9]*[a-z][a-z\\.\\d\\-]+)\\.(?:[0-9]*[a-z][a-z\\-]+))(?![\\w\\.])",response.read())
+for domain in domains:
+  mt.addEntity("maltego.Domain", domain)
 mt.returnOutput()
