f4c308e2ab17ea1611b4f13b867df34ed3a2f3db
SudhanshuC <sudhanshuchauhan007@yahoo.com>
2014-05-15 18:43:04 +0530

    Update mtgxtocsv.py

--- mtgxtocsv.py
+++ mtgxtocsv.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # This script can be used to generate a CSV file from the mtgx file. A mtgx file is a file created by saving the Maltego graph.
-# Currently it only extracts the entities and not the details of the edges.
+# It extracts the entities, their type and entity parent(s).
+#! /usr/bin/python
 from BeautifulSoup import BeautifulSoup
 import csv
 import zipfile
@@ -11,16 +12,39 @@ path=str(zp.namelist()[0])
 data=zp.read(path)
 xmlbs=BeautifulSoup(data)
 psxml=xmlbs.graph.findAll("node")
-entity=[]
-entvalue=[]
+key=xmlbs.findAll("key")
+edge=xmlbs.findAll("edge")
+xkey=""
+el=[]
+edgelink=["Entity Parent(s)"]
+entity=["Entity Type"]
+entvalue=["Entity Value"]
+link=["Entity ID"]
+lnk=""
 outcsv=open('maltegoop.csv', 'wb')
 csvwriter = csv.writer(outcsv)
+print ""
+
+for k in key:
+  if (k["for"]=="edge"):
+    if (k["attr.name"]=="MaltegoLink"):
+	 xkey=k["id"]
+	 break
+  	 
 for node in psxml:
+  lnk=node["id"]
+  link.append(lnk)
+  for ed in edge:
+    if ed["target"]==lnk:
+	  el.append(str(ed["source"]))
+  edgelink.append(el)
+  el=[]  
   ent=node.findAll("mtg:maltegoentity")[0]["type"]
   entity.append(ent)
   nod=node.findAll("mtg:value")[0].contents
   for nd in nod:
     entvalue.append(nd)
-for val in (entity, entvalue):
+	
+for val in (link, entity, entvalue, edgelink):
     csvwriter.writerow(val)
 outcsv.close()
