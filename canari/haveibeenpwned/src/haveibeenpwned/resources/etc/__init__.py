7090b0926870d84d6c0b5b80ff9b0801597e59ed
Christian Heinrich <christian.heinrich@cmlh.id.au>
2014-10-06 17:25:02 +1100

    Add canari create-package files
    $ canari create-package haveibeenpwned
    Welcome to the Canari transform package wizard.
    Would you like to specify authorship information? [Y/n]: Y
    Project description []: cmlh.id.au
    Generate an example transform? [Y/n]: Y
    Author name [cmlh]: Christian Heinrich
    Author email []: christian.heinrich@cmlh.id.au
    Maintainer name [Christian Heinrich]:
    Are you satisfied with this information? [Y/n]: Y
    creating skeleton in haveibeenpwned
    creating directory haveibeenpwned
    creating directory haveibeenpwned/src
    creating directory haveibeenpwned/maltego
    creating directory haveibeenpwned/src/haveibeenpwned
    creating directory haveibeenpwned/src/haveibeenpwned/transforms
    creating directory haveibeenpwned/src/haveibeenpwned/transforms/common
    creating directory haveibeenpwned/src/haveibeenpwned/resources
    creating directory haveibeenpwned/src/haveibeenpwned/resources/etc
    creating directory haveibeenpwned/src/haveibeenpwned/resources/images
    creating directory haveibeenpwned/src/haveibeenpwned/resources/external
    creating directory haveibeenpwned/src/haveibeenpwned/resources/maltego
    creating file haveibeenpwned/.canari...
    creating file haveibeenpwned/setup.py...
    creating file haveibeenpwned/README.md...
    creating file haveibeenpwned/MANIFEST.in...
    creating file haveibeenpwned/src/haveibeenpwned/__init__.py...
    creating file haveibeenpwned/src/haveibeenpwned/resources/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/resources/etc/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/resources/images/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/resources/external/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/resources/maltego/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/resources/etc/haveibeenpwned.conf...
    creating file
    haveibeenpwned/src/haveibeenpwned/transforms/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/transforms/helloworld.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/transforms/common/__init__.py...
    creating file
    haveibeenpwned/src/haveibeenpwned/transforms/common/entities.py...
    done!

---
+++ canari/haveibeenpwned/src/haveibeenpwned/resources/etc/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__author__ = 'Christian Heinrich'
+__copyright__ = 'Copyright 2014, Haveibeenpwned Project'
+__credits__ = []
+
+__license__ = 'GPL'
+__version__ = '0.1'
+__maintainer__ = 'Christian Heinrich'
+__email__ = 'christian.heinrich@cmlh.id.au'
+__status__ = 'Development'
