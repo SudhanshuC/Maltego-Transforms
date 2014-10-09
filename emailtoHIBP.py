380e44634a26fcb6ae799a184bf9f8fadb7df523
Christian Heinrich <christian.heinrich@cmlh.id.au>
2014-10-09 12:10:45 +1100

    Refactor PEP 8

--- emailtoHIBP.py
+++ emailtoHIBP.py
@@ -7,21 +7,26 @@
 #It uses the API provided by https://haveibeenpwned.com/
 #Special Thanks to Troy Hunt - http://www.troyhunt.com/
 #For MaltegoTransform library and Installation guidelines go to http://www.paterva.com/web6/documentation/developer-local.php
-   
-from MaltegoTransform import *
-import sys
+
+import sys  # PEP 8 http://legacy.python.org/dev/peps/pep-0008/#imports
 import urllib2
+
+from MaltegoTransform import *
+
+HIBP = "https://haveibeenpwned.com/api/breachedaccount/" # PEP 8 http://legacy.python.org/dev/peps/pep-0008/#constants
+
 mt = MaltegoTransform()
 mt.parseArguments(sys.argv)
-email=mt.getValue()
+email = mt.getValue()  # PEP 8 http://legacy.python.org/dev/peps/pep-0008/#other-recommendations
 mt = MaltegoTransform()
-hibp="https://haveibeenpwned.com/api/breachedaccount/"
-getrequrl=hibp+email
-try:
-response = urllib2.urlopen(getrequrl)
-for rep in response:
-mt.addEntity("maltego.Phrase","Pwned at " + rep)
-except:
-print ""
-mt.returnOutput()
+getrequrl = HIBP + email
 
+try:
+    response = urllib2.urlopen(getrequrl)
+    for rep in response:
+        mt.addEntity("maltego.Phrase","Pwned at " + rep)
+
+except:
+        print ""
+        
+mt.returnOutput()
